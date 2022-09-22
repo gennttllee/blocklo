1eda164cf03dd533119999ccf010fc9a42305d9f
gennttllee <udooto72@gmail.com>
2022-09-22 18:03:19 +0100

    fiex element size in landing page

--- src/components/app/small/Element.js
+++ src/components/app/small/Element.js
@@ -12,7 +12,7 @@ const Element = ({ content, tag }) => {
         return (
             <div className="flex w-fit items-center gap-2 rounded-lg h-4/5  border border-[#ffffff79]">
                 <p className="whitespace-pre-wrap leading-none text-right">{tag}</p>
-                <p className="text-3xl font-bold w-3/4">{content}</p>
+                <p className="text-xl sm:text-3xl font-bold w-3/4">{content}</p>
             </div>
         )
     }
