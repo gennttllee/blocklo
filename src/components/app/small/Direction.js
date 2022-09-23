8e840cd9e68071d5c425e0753b3c7e5b4a86a27d
gennttllee <udooto72@gmail.com>
2022-09-23 22:05:57 +0100

    minor tweak

--- src/components/app/small/Direction.js
+++ src/components/app/small/Direction.js
@@ -31,15 +31,15 @@ const Direction = ({ row, icon, y1, z1, l1, h1 }) => {
             <>
                 <div className="hidden  lg:flex gap-10 items-center justify-center mx-auto my-[-1px] min-h-[20rem] max-h-[27rem] h-[22rem]  ">
                     <SmallBox h1={y1} p1={z1} row={row} />
-                    <div className='bg-gradient-to-b from-[#2f79f939] to-[#3daefa34] w-[1rem] h-full flex items-center'>
-                        <FontAwesomeIcon className="text-[#3daefa34] text-7xl mr-[-1rem]" icon={faCaretRight} />
+                    <div className='bg-[#2a2a55] w-[1rem] h-full flex items-center'>
+                        <FontAwesomeIcon className="text-[#2a2a55] text-7xl mr-[-1rem]" icon={faCaretRight} />
                     </div>
                     <Box icon={icon} h1={h1} l1={l1} />
                 </div>
 
                 <div className=" flex lg:hidden gap-7 items-center mx-auto  w-11/12">
-                    <div className='bg-gradient-to-b from-[#2f79f939] to-[#3daefa34] w-[1rem] h-[28rem] flex items-center'>
-                        <FontAwesomeIcon className="text-[#3daefa34] text-7xl mr-[-1rem]" icon={faCaretRight} />
+                    <div className='bg-[#2a2a55] w-[1rem] h-[28rem] flex items-center'>
+                        <FontAwesomeIcon className="text-[#2a2a55] text-7xl mr-[-1rem]" icon={faCaretRight} />
                     </div>
                     <div className="flex flex-col gap-y-7">
                         <SmallBox h1={y1} p1={z1} row={true} />
