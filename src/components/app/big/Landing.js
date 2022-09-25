c869cee16ec1c33ce5acd094dcb7839fd0dd1a54
gennttllee <udooto72@gmail.com>
2022-09-25 11:50:41 +0100

    fixed landing page

--- src/components/app/big/Landing.js
+++ src/components/app/big/Landing.js
@@ -6,15 +6,15 @@ import Icons from '../small/icons'
 
 const Landing = () => {
     return (
-        <section className="bg-cover-pic bg-center bg-cover h-[42rem] lg:h-[100vh] relative flex items-center justify-center">
-            <div className='text-white block w-4/5  2xl:w-2/4' >
-                <FontAwesomeIcon className='block text-center w-full text-5xl 2xl:text-7xl mb-4' icon={faCloud} />
-                <h1 className=' text-3xl lg:text-5xl xl:text-7xl font-bold mb-4'>
-                    <span className='block text-center mb-3'>Integrated Web3</span>
-                    <span className='text-center block'>Ecosystem Built To Scale</span>
+        <section className="bg-cover-pic bg-center bg-cover h-screen relative flex items-center justify-center">
+            <div className='text-white w-full mt-[-10rem]' >
+                <FontAwesomeIcon className='block text-center w-full text-5xl md:text-6xl 2xl:text-7xl mb-4' icon={faCloud} />
+                <h1 className=' text-3xl md:text-5xl  lg:text-6xl font-bold mb-4 text-center'>
+                    <span className='block mb-2 md:mb-4'>Integrated Web3</span>
+                    <span className='block mb-2'>Ecosystem Built To Scale</span>
                 </h1>
-                <p className='font-normal text-lg lg:text-3xl break-all text-center leading-relaxed'>Cloudax is a decentralized and scalable ecosystem built to power the next generation of innovative web3 products</p>
-                <div className='mx-auto mt-10 w-fit flex flex-col md:flex-row gap-4'>
+                <p className='text-[#ffffffd9] w-11/12 leading-relaxed sm:w-3/4 2xl:w-2/4 mx-auto font-normal text-lg md:text-2xl md:leading-loose lg:text-3xl break-all text-center sm:leading-loose xl:leading-relaxed'>Cloudax is a decentralized and scalable ecosystem built to power the next generation of innovative web3 products</p>
+                <div className='mx-auto mt-10 w-fit flex flex-col md:flex-row gap-6'>
                     <button className='h-[58px] md:h-[68px] text-base md:text-2xl w-[240px] rounded-lg bg-gradient-to-r from-[#2F79F9] to-[#3DAEFA]'> <FontAwesomeIcon className='mr-2' icon={faCopy} />Join Presale</button>
                     <button className='border-2 border-white h-[58px] md:h-[68px] text-base md:text-2xl w:[193px] md:w-[240px] rounded-lg '>
                         <FontAwesomeIcon className='mr-2' icon={faPaperPlane} />
@@ -22,7 +22,7 @@ const Landing = () => {
                 </div>
             </div>
 
-            <div className='bg-[#4D53DD] h-16 md:h-24 w-full text-white absolute bottom-0 top-auto "'>
+            <div className='bg-[#4D53DD] h-20 md:h-28 py-3 md:py-4 w-full text-white absolute bottom-0 top-auto "'>
                 <div className="w-11/12 mx-auto h-full flex justify-between items-center">
                     <div className="flex gap-2 h-full items-center">
                         <Element tag='Public Price' content='$0.0025' />
