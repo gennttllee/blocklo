cbb57b0c16a495ab67877736b2f109bbd432a650
gennttllee <udooto72@gmail.com>
2022-09-25 18:34:05 +0100

    added animation on landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,14 @@ module.exports = {
     extend: {
       backgroundImage: { 'cover-pic': "url('/public/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')" },
       keyframes: {
-        "maker": {
-          from: {
-            background: "url('/public/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')"
-          },
-          to: {
-            background: "url('/public/Mask group.png')"
-          }
+        maker: {
+          "0%": { backgroundImage: "url('/public/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')"},
+          "50%": { backgroundImage: "url('/public/Mask group.png')"},
+          "10%": { backgroundImage: "url('/public/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')"},
         },
-        animation: {
-          "makers": " maker 1s ease-in-out infinite",
-        }
+      },
+      animation: {
+        makers: " maker 9s ease-in-out infinite",
       }
     },
   },
