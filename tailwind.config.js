77a80d9e5ba5febdc94ed95dbf4ef855d50a1ec3
gennttllee <udooto72@gmail.com>
2022-09-26 01:11:30 +0100

    changed background image size

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: { 'cover-pic': "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')" },
+      backgroundImage: { 'cover-pic': "url('/src/images/blue.jpeg')" },
       keyframes: {
         maker: {
-          "0%": { backgroundImage: "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')",  backgroundSize: "cover", backgroundPosition : "center"},
-          "50%": { backgroundImage: "url('/src/images/Mask group.png')", backgroundSize: "cover", backgroundPosition : "center"},
-          "10%": { backgroundImage: "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')", backgroundSize: "cover", backgroundPosition : "center"},
+          "0%": { backgroundImage: "url('/src/images/blue.jpeg')",  backgroundSize: "cover", backgroundPosition : "center"},
+          "50%": { backgroundImage: "url('/src/images/monkey.jpeg')", backgroundSize: "cover", backgroundPosition : "center"},
+          "10%": { backgroundImage: "url('/src/images/blue.jpeg')", backgroundSize: "cover", backgroundPosition : "center"},
         },
       },
       animation: {
