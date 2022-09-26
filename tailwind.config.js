b6c89fea246a3fd964b3054dd76f0bd5d03f6d49
gennttllee <udooto72@gmail.com>
2022-09-26 08:15:36 +0100

    added 2 bg colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ module.exports = {
       backgroundImage: { 'cover-pic': "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')" },
       keyframes: {
         maker: {
-          "0%": { backgroundImage: "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')",  backgroundColor : '#2F79F9'},
-          "50%": { backgroundImage: "url('/src/images/Mask group.png')", backgroundColor : '#3DAEFA'},
-          "10%": { backgroundImage: "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')", backgroundColor : '#2F79F9'},
+          "0%": { backgroundImage: "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')",  backgroundColor : '#4D53DD'},
+          "50%": { backgroundImage: "url('/src/images/Mask group.png')", backgroundColor : '#2F79F9'},
+          "10%": { backgroundImage: "url('/src/images/guerrillabuzz-crypto-pr-JlijbOtSWuw-unsplash 1.png')", backgroundColor : '#4D53DD'},
         },
       },
       animation: {
